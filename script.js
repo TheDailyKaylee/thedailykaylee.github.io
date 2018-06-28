a143b8ee651d53876c72e5be18e657eb18ea000c
TheDailyKaylee <40675848+TheDailyKaylee@users.noreply.github.com>
2018-06-28 14:23:25 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function login() {
+var username = document.getElementById('username').value;
+var password =  document.getElementById('password').value;
+if ((username=='ellabc23'&& password=='unicorn')||(username=='Cami' && password=='pixie')||(username=='TheNovelEnchantress' && password=='dragon')||(username=='Katelyn'&& password=='alicorn')||(username=='Madison'&& password=='phoenix')){
+window.location.href='home.html';
+}
+}                                                                    
