61f1d93dd3836305908c5f3b74a4fe18c51b18e0
TheDailyKaylee <40675848+TheDailyKaylee@users.noreply.github.com>
2018-09-08 12:37:33 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+
+function login() {
+var username = document.getElementById('username').value;
+var password =  document.getElementById('password').value;
+if ((username=='Kaylee'&& password=='WordsareMagic')||(username=='readmagic' && password=='frodo')||(username=='TheNovelEnchantress' && password=='dragon')||(username=='Katelyn'&& password=='alicorn')||(username=='Madison'&& password=='phoenix')){
+window.location.href='index.html';
+}
+} 
